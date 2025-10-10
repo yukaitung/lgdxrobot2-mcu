a02aab602527da3d260d730386f8dee96781531c
Kai Tung Yu <sam@bristolgram.uk>
2025-10-10 23:22:55 +0100

    prevent mcu fault

--- Src/main.c
+++ Src/main.c
@@ -55,7 +55,7 @@ TIM_HandleTypeDef htim9;
 
 /* USER CODE BEGIN PV */
 // Data
-McuData mcu_data = {0};;
+McuData mcu_data = {0};
 
 // Power Monitoring
 enum __batteries {
@@ -613,12 +613,6 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOB, DR1AIN1_Pin|DRxSTBY_Pin|DR1BIN1_Pin|DR1BIN2_Pin
                           |D1_Pin|DR2AIN1_Pin|DR2AIN2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PC13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
   /*Configure GPIO pins : DR2BIN1_Pin DR2BIN2_Pin */
   GPIO_InitStruct.Pin = DR2BIN1_Pin|DR2BIN2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
@@ -641,18 +635,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PB12 PB13 PB14 */
-  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PA13 PA14 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
