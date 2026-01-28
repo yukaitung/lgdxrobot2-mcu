decedf9003ca16336bee4533063db71100337281
Kai Tung Yu <sam@bristolgram.uk>
2026-01-28 08:26:18 +0000

    remove code

--- Inc/motor.h
+++ Inc/motor.h
@@ -15,7 +15,6 @@ float MOTOR_Get_Target_Velocity(int motor);
 float MOTOR_Get_Pid_Speed(int level);
 float MOTOR_Get_Maximum_Speed(int motor);
 float MOTOR_Get_Pid(int motor, int level, int k);
-bool MOTOR_Get_Emergency_Stop_Status(int type);
 void MOTOR_Set_Ik(float velocity_x, float velocity_y, float velocity_w);
 void MOTOR_Set_Single_Motor(int motor, float velocity);
 void MOTOR_Set_Temporary_Pid_Speed(float level1, float level2, float level3);
