6ea45bbcffada44758d3dd41bf26246194cfdee8
Kai Tung Yu <sam@bristolgram.uk>
2026-01-29 22:49:34 +0000

    bias

--- Src/imu.c
+++ Src/imu.c
@@ -4,7 +4,7 @@
 #include "imu.h"
 #include <stdint.h>
 
-#define CABLICATE_COUNT 100
+#define CABLICATE_COUNT 1000
 
 const uint8_t _gyro_precision = GYRO_500_DPS;
 const uint8_t _accel_precision = ACCEL_2G;
@@ -21,6 +21,9 @@ int _current_step = done;
 SPI_HandleTypeDef *_hspi;
 McuImuDof _imu_data = {0};
 uint8_t _buffer[12] = {0};
+
+float accel_offset[3] = {0};
+float gyro_offset[3] = {0};
 
 /*
  * Private Functions
@@ -70,13 +73,13 @@ float _get_accel(uint8_t high, uint8_t low)
   int16_t value = (high << 8) | low;
   switch (_accel_precision) {
     case ACCEL_2G:
-      return (float)value * (2 / 32768.0) * G_TO_M_S2;
+      return (float)value * (2 / 32768.0);
     case ACCEL_4G:
-      return (float)value * (4 / 32768.0) * G_TO_M_S2;
+      return (float)value * (4 / 32768.0);
     case ACCEL_8G:
-      return (float)value * (8 / 32768.0) * G_TO_M_S2;
+      return (float)value * (8 / 32768.0);
     case ACCEL_16G:
-      return (float)value * (16 / 32768.0) * G_TO_M_S2;
+      return (float)value * (16 / 32768.0);
     default:
       return 0.0;
   }
@@ -87,13 +90,13 @@ float _get_gyro(uint8_t high, uint8_t low)
   int16_t value = (high << 8) | low;
   switch (_gyro_precision) {
     case GYRO_250_DPS:
-      return (float)value * (250 / 32768.0) * DEG_TO_RAD;
+      return (float)value * (250 / 32768.0);
     case GYRO_500_DPS:
-      return (float)value * (500 / 32768.0) * DEG_TO_RAD;
+      return (float)value * (500 / 32768.0);
     case GYRO_1000_DPS:
-      return (float)value * (1000 / 32768.0) * DEG_TO_RAD;
+      return (float)value * (1000 / 32768.0);
     case GYRO_2000_DPS:
-      return (float)value * (2000 / 32768.0) * DEG_TO_RAD;
+      return (float)value * (2000 / 32768.0);
     default:
       return 0.0;
   }
@@ -102,52 +105,31 @@ float _get_gyro(uint8_t high, uint8_t low)
 void _cablicate()
 {
   uint8_t addr = ACCEL_XOUT_H | 0x80;
-  uint8_t buffer[12] = {0};
-  int32_t acc_accel[3] = {0};
-  int32_t acc_gyro[3] = {0};
-  int16_t cab_accel[3] = {0};
-  int16_t cab_gyro[3] = {0};
+  float acc_accel[3] = {0};
+  float acc_gyro[3] = {0};
 
   _select_bank(0);
   for (int i = 0; i < CABLICATE_COUNT; i++) 
   {
     _select();
     HAL_SPI_Transmit(_hspi, &addr, 1, TIMEOUT_MS);
-    HAL_SPI_Receive(_hspi, buffer, 12, TIMEOUT_MS);
+    HAL_SPI_Receive(_hspi, _buffer, 12, TIMEOUT_MS);
     _unselect();
-    acc_accel[0] += (int16_t)(buffer[0] << 8 | buffer[1]);
-    acc_accel[1] += (int16_t)(buffer[2] << 8 | buffer[3]);
-    acc_accel[2] += (int16_t)(buffer[4] << 8 | buffer[5]);
-    acc_gyro[0] += (int16_t)(buffer[6] << 8 | buffer[7]);
-    acc_gyro[1] += (int16_t)(buffer[8] << 8 | buffer[9]);
-    acc_gyro[2] += (int16_t)(buffer[10] << 8 | buffer[11]);
-    HAL_Delay(2);
+    acc_accel[0] += _get_accel(_buffer[0], _buffer[1]);
+    acc_accel[1] += _get_accel(_buffer[2], _buffer[3]);
+    acc_accel[2] += _get_accel(_buffer[4], _buffer[5]);
+    acc_gyro[0] += _get_gyro(_buffer[6], _buffer[7]);
+    acc_gyro[1] += _get_gyro(_buffer[8], _buffer[9]);
+    acc_gyro[2] += _get_gyro(_buffer[10], _buffer[11]);
+    HAL_Delay(1);
   }
-  cab_accel[0] = -(int16_t)(acc_accel[0] / CABLICATE_COUNT);
-  cab_accel[1] = -(int16_t)(acc_accel[1] / CABLICATE_COUNT);
-  cab_accel[2] = -(int16_t)(acc_accel[2] / CABLICATE_COUNT);
-  cab_accel[0] = (cab_accel[0] << 1) & 0xFFFE;
-  cab_accel[1] = (cab_accel[1] << 1) & 0xFFFE;
-  cab_accel[2] = (cab_accel[2] << 1) & 0xFFFE;
-  cab_gyro[0] = -(int16_t)(acc_gyro[0] / CABLICATE_COUNT);
-  cab_gyro[1] = -(int16_t)(acc_gyro[1] / CABLICATE_COUNT);
-  cab_gyro[2] = -(int16_t)(acc_gyro[2] / CABLICATE_COUNT);
-
-  _select_bank(1);
-  _write(XA_OFFS_H, (uint8_t)(cab_accel[0] >> 8));
-  _write(XA_OFFS_L, (uint8_t)(cab_accel[0]));
-  _write(YA_OFFS_H, (uint8_t)(cab_accel[1] >> 8));
-  _write(YA_OFFS_L, (uint8_t)(cab_accel[1]));
-  _write(ZA_OFFS_H, (uint8_t)(cab_accel[2] >> 8));
-  _write(ZA_OFFS_L, (uint8_t)(cab_accel[2]));
-
-  _select_bank(2);
-  _write(XG_OFFS_USRH, (uint8_t)(cab_gyro[0] >> 8));
-  _write(XG_OFFS_USRL, (uint8_t)(cab_gyro[0]));
-  _write(YG_OFFS_USRH, (uint8_t)(cab_gyro[1] >> 8));
-  _write(YG_OFFS_USRL, (uint8_t)(cab_gyro[1]));
-  _write(ZG_OFFS_USRH, (uint8_t)(cab_gyro[2] >> 8));
-  _write(ZG_OFFS_USRL, (uint8_t)(cab_gyro[2]));
+  accel_offset[0] = acc_accel[0] / CABLICATE_COUNT;
+  accel_offset[1] = acc_accel[1] / CABLICATE_COUNT;
+  accel_offset[2] = acc_accel[2] / CABLICATE_COUNT;
+  accel_offset[2] -= 1;
+  gyro_offset[0] = acc_gyro[0] / CABLICATE_COUNT;
+  gyro_offset[1] = acc_gyro[1] / CABLICATE_COUNT;
+  gyro_offset[2] = acc_gyro[2] / CABLICATE_COUNT;
 }
 
 void _imu_step_process()
@@ -193,12 +175,12 @@ void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef * hspi)
     _unselect();
     if (_current_step == get_accel_gyro_read)
     {
-      _imu_data.accelerometer.x = _get_accel(_buffer[0], _buffer[1]);
-      _imu_data.accelerometer.y = _get_accel(_buffer[2], _buffer[3]);
-      _imu_data.accelerometer.z = _get_accel(_buffer[4], _buffer[5]);
-      _imu_data.gyroscope.x = _get_gyro(_buffer[6], _buffer[7]);
-      _imu_data.gyroscope.y = _get_gyro(_buffer[8], _buffer[9]);
-      _imu_data.gyroscope.z = _get_gyro(_buffer[10], _buffer[11]);
+      _imu_data.accelerometer.x = _get_accel(_buffer[0], _buffer[1]) * G_TO_M_S2;
+      _imu_data.accelerometer.y = _get_accel(_buffer[2], _buffer[3]) * G_TO_M_S2;
+      _imu_data.accelerometer.z = _get_accel(_buffer[4], _buffer[5]) * G_TO_M_S2;
+      _imu_data.gyroscope.x = (_get_gyro(_buffer[6], _buffer[7]) - gyro_offset[0]) * DEG_TO_RAD;
+      _imu_data.gyroscope.y = (_get_gyro(_buffer[8], _buffer[9]) - gyro_offset[1]) * DEG_TO_RAD;
+      _imu_data.gyroscope.z = (_get_gyro(_buffer[10], _buffer[11]) - gyro_offset[2]) * DEG_TO_RAD;
     }
     _current_step++;
     _imu_step_process();
@@ -216,7 +198,7 @@ void IMU_Init(SPI_HandleTypeDef *hspi)
   // 1. Initialise
     // Reset
   _select_bank(0);
-  _write(PWR_MGMT_1, 0xC1);
+  _write(PWR_MGMT_1, 0x80);
   HAL_Delay(100);
   _write(PWR_MGMT_1, 0x01);
 
@@ -224,17 +206,13 @@ void IMU_Init(SPI_HandleTypeDef *hspi)
   _select_bank(2);
   _write(ODR_ALIGN_EN, 0x01);
 
-  _write(GYRO_SMPLRT_DIV, 0x01);
-  _write(GYRO_CONFIG_1, (GYRO_1000_DPS << 1) | 0x01);
+  //_write(GYRO_SMPLRT_DIV, 0x01);
+  _write(GYRO_CONFIG_1, (6 << 3) | (_gyro_precision << 1) | 0x01);
 
 
-  _write(ACCEL_SMPLRT_DIV_1, 0x00);
+  _write(ACCEL_SMPLRT_DIV_1, 0x0A);
   _write(ACCEL_SMPLRT_DIV_2, 0x00);
-  _write(ACCEL_CONFIG, (ACCEL_16G << 1) | 0x01);
-
-  _cablicate();
-  _write(GYRO_CONFIG_1, (_gyro_precision << 1) | 0x01);
-  _write(ACCEL_CONFIG, (_accel_precision << 1) | 0x01);
+  _write(ACCEL_CONFIG, (6 << 3) | (_accel_precision << 1) | 0x01);
 
     // Enable I2C
   _select_bank(0);
@@ -243,16 +221,31 @@ void IMU_Init(SPI_HandleTypeDef *hspi)
   _select_bank(2);
   _write(USER_CTRL, config);
 
+  _cablicate();
+
   IMU_Read_Start();
 }
 
 void IMU_Read_Start()
 {
+  _select_bank(0);
+  uint8_t addr = ACCEL_XOUT_H | 0x80;
+  _select();
+  HAL_SPI_Transmit(_hspi, &addr, 1, TIMEOUT_MS);
+  HAL_SPI_Receive(_hspi, _buffer, 12, TIMEOUT_MS);
+  _unselect();
+  _imu_data.accelerometer.x = (_get_accel(_buffer[0], _buffer[1]) - accel_offset[0]) * G_TO_M_S2;
+  _imu_data.accelerometer.y = (_get_accel(_buffer[2], _buffer[3]) - accel_offset[1]) * G_TO_M_S2;
+  _imu_data.accelerometer.z = (_get_accel(_buffer[4], _buffer[5]) - accel_offset[2]) * G_TO_M_S2;
+  _imu_data.gyroscope.x = (_get_gyro(_buffer[6], _buffer[7]) - gyro_offset[0]) * DEG_TO_RAD;
+  _imu_data.gyroscope.y = (_get_gyro(_buffer[8], _buffer[9]) - gyro_offset[1]) * DEG_TO_RAD;
+  _imu_data.gyroscope.z = (_get_gyro(_buffer[10], _buffer[11]) - gyro_offset[2]) * DEG_TO_RAD;
+/*
  if (_current_step == done)
  {
     _current_step = select_bank_0_addr;
     _imu_step_process();
- }
+ }*/
 }
 
 McuImuDof IMU_Get_Data()
